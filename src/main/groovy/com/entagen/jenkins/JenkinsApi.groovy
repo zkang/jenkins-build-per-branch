1c3d096f105627b49969d90548db540086c714ff
ZiLing Kang <zkang@contentanalyst.com>
2014-01-30 14:02:07 -0500

    CAAT-2607 fix build option not available

--- src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
@@ -61,10 +61,7 @@ class JenkinsApi {
         String missingJobConfig = configForJob(missingJob, templateJobs)
         TemplateJob templateJob = missingJob.templateJob
 
-        //Copy job with jenkins copy job api, this will make sure jenkins plugins get the call to make a copy if needed (promoted builds plugin needs this)
-        post('createItem', missingJobConfig, [name: missingJob.jobName, mode: 'copy', from: templateJob.jobName], ContentType.XML)
-
-        post('job/' + missingJob.jobName + "/config.xml", missingJobConfig, [:], ContentType.XML)
+        post('createItem', missingJobConfig, [name: missingJob.jobName], ContentType.XML)
     }
 
     void startJob(ConcreteJob job) {
